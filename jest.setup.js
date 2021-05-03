bae2721d0378a329691fc88e23d468189c34eb03
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-03 13:59:50 +0100

    Refactor variables definition

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,7 @@ import { config } from '@vue/test-utils'
 // Currently vue-jest doesn't support css modules
 // Define proxy handler for $style object to mock the selectors
 const get = (target, propName) => propName
+const mock$style = new Proxy({}, { get })
 
 config.global.mocks = config.global.mocks || {}
-config.global.mocks.$style = new Proxy({}, { get })
+config.global.mocks.$style = mock$style
