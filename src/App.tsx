7257f118c162379b0e1e25ecefcb7f0b025ceec2
Konrad Nowakowski <konrad.x92@gmail.com>
2022-03-17 20:01:24 +0000

    Replace styling and app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="md:container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="md:container mx-auto">
+      <header className="text-lg">
+        <h1>TODO Application</h1>
       </header>
     </div>
   );
